a0dbe04478b4280a4b5090259eb44df64ce5e122
MNastri <marcellonastri@gmail.com>
2021-10-04 00:04:32 -0300

    Incluir algumas funções para o tabuleiro
    Incluir a fução que marca uma célula com o valor do jogador escolhido
    Incluir a função que verifica se uma célula está disponível para um jogador
    Incluir a função que verifica se o tabuleiro está cheio

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import numpy as np
 import sys
 from typing import Union
 
@@ -6,6 +7,9 @@ from pygame.surface import SurfaceType
 
 WIDTH = 600
 HEIGHT = 600
+
+BOARD_ROWS = 3
+BOARD_COLUMNS = 3
 
 RED = (255, 0, 0)
 BG_COLOR = (28, 170, 156)
@@ -28,12 +32,72 @@ def draw_playing_grid(surface: Union[pygame.Surface, SurfaceType]) -> None:
                          LINE_WIDTH)
 
 
+def mark_cell(board, row, column, player):
+    board[row][column] = player
+
+
+def is_cell_available(board, row, column) -> bool:
+    return board[row][column] == 0
+
+
+def is_board_full(board) -> bool:
+    for row in range(BOARD_ROWS):
+        for column in range(BOARD_COLUMNS):
+            if board[row][column] == 0:
+                return False
+    return True
+
+
 def main_loop():
     pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     screen.fill(BG_COLOR)
     pygame.display.set_caption('Tic Tac Toe')
     draw_playing_grid(screen)
+    board = np.zeros((BOARD_ROWS, BOARD_COLUMNS))
+
+    """
+    # test
+    is_board_full(board)
+    mark_cell(board, 0, 0, 1)
+    print(board)
+
+    is_board_full(board)
+    mark_cell(board, 0, 1, 1)
+    print(board)
+
+    is_board_full(board)
+    mark_cell(board, 0, 2, 1)
+    print(board)
+
+    is_board_full(board)
+    mark_cell(board, 1, 0, 1)
+    print(board)
+
+    is_board_full(board)
+    mark_cell(board, 1, 1, 1)
+    print(board)
+
+    is_board_full(board)
+    mark_cell(board, 1, 2, 1)
+    print(board)
+
+    is_board_full(board)
+    mark_cell(board, 2, 0, 1)
+    print(board)
+
+    is_board_full(board)
+    mark_cell(board, 2, 1, 1)
+    print(board)
+
+    is_board_full(board)
+    mark_cell(board, 2, 2, 1)
+    print(board)
+
+    is_board_full(board)
+    # end test
+    """
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
