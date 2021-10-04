27962264f6b61dc28459948cd031002192505ce2
MNastri <marcellonastri@gmail.com>
2021-10-03 23:20:41 -0300

    Criar grid de jogo

--- main.py
+++ main.py
@@ -6,12 +6,31 @@ HEIGHT = 600
 
 RED = (255, 0, 0)
 BG_COLOR = (28, 170, 156)
+LINE_COLOR = (23, 145, 135)
+
+LINE_WIDTH = 15
+
+
+def draw_playing_grid(surface) -> None:
+    for ii in range(1, 3):
+        pygame.draw.line(surface,
+                         LINE_COLOR,
+                         (0, ii * WIDTH/3),
+                         (HEIGHT, ii * WIDTH/3),
+                         LINE_WIDTH)
+        pygame.draw.line(surface,
+                         LINE_COLOR,
+                         (ii * HEIGHT/3, 0),
+                         (ii * HEIGHT/3, WIDTH),
+                         LINE_WIDTH)
+
 
 def main_loop():
     pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     screen.fill(BG_COLOR)
     pygame.display.set_caption('Tic Tac Toe')
+    draw_playing_grid(screen)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
