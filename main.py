d600cd4524ac9e01362d28f2a536cc26e5504e94
MNastri <marcellonastri@gmail.com>
2021-10-03 22:57:54 -0300

    Criar a janela do jogo

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import pygame
+import sys
+
+WIDTH = 600
+HEIGHT = 600
+
+
 def main_loop():
-    pass
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
 
 if __name__ == "__main__":
