388732047d747a62e8717628b91146e00f37a2d9
MNastri <marcellonastri@gmail.com>
2021-10-03 23:00:02 -0300

    Nomear janela do jogo

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ HEIGHT = 600
 def main_loop():
     pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption('Tic Tac Toe')
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
